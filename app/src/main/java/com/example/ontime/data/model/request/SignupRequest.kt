67c9e6173bed42ec858a69db780e763a54464d62
pp.navillera <pp.navillera@gmail.com>
2024-11-18 00:43:10 +0900

    Feat: define data class for Signup Request and Response

---
+++ app/src/main/java/com/example/ontime/data/model/request/SignupRequest.kt
@@ -0,0 +1,8 @@
+package com.example.ontime.data.model.request
+
+// SignupRequest.kt
+data class SignupRequest(
+    val phoneNumber: String,
+    val name: String,
+    val password: String
+)
---
+++ app/src/main/java/com/example/ontime/data/model/response/SignupResponse.kt
@@ -0,0 +1,9 @@
+package com.example.ontime.data.model.response
+
+
+// SignupResponse.kt
+data class SignupResponse(
+    val accessToken: String,
+    val refreshToken: String,
+    // 기타 필요한 사용자 정보
+)
