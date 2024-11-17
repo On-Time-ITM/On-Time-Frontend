48f648926ffe0429b99cd57ce5e59400cad1cedf
pp.navillera <pp.navillera@gmail.com>
2024-11-18 00:45:23 +0900

    Feat: configure Retrofit Instance

---
+++ app/src/main/java/com/example/ontime/data/api/ApiClient.kt
@@ -0,0 +1,19 @@
+package com.example.ontime.data.api
+
+import AuthApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private val BASE_URL = "http://10.0.2.2:8080"
+
+//    private val okHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+//        level =
+//    })
+
+    private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+        GsonConverterFactory.create()
+    ).build()
+
+    val authApi: AuthApi = retrofit.create(AuthApi::class.java)
+}
