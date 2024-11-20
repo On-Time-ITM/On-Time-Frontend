4bc2bf603c79f3e58506b2cbc0ffc6aa85b0a55e
pp.navillera <pp.navillera@gmail.com>
2024-11-20 12:11:37 +0900

    Feat: define data class for Friends Request and Response

---
+++ app/src/main/java/com/example/ontime/data/model/request/InviteFriendsRequest.kt
@@ -0,0 +1,6 @@
+package com.example.ontime.data.model.request
+
+
+data class InviteFriendsRequest(
+    val friendIds: List<String>
+)
---
+++ app/src/main/java/com/example/ontime/data/model/response/FriendResponse.kt
@@ -0,0 +1,10 @@
+package com.example.ontime.data.model.response
+
+
+data class FriendResponse(
+    val id: String,
+    val name: String,
+    val phoneNumber: String,
+    val tardinessRate: Float,
+    val profileImage: String
+)
