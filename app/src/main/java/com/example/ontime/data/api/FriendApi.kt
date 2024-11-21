1fb232d7e0516420fb34d00c61a71d45bc2ab5f9
pp.navillera <pp.navillera@gmail.com>
2024-11-21 22:40:29 +0900

    Feat: define POST addFriends API

--- app/src/main/java/com/example/ontime/data/api/FriendApi.kt
+++ app/src/main/java/com/example/ontime/data/api/FriendApi.kt
@@ -1,10 +1,17 @@
 package com.example.ontime.data.api
 
+import com.example.ontime.data.model.request.AddFriendRequest
 import com.example.ontime.data.model.response.FriendResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface FriendApi {
     @GET("/friends")
     suspend fun getFriends(): Response<List<FriendResponse>>
+
+    @POST("/api/v1/friendship")
+    suspend fun addFriend(@Body request: AddFriendRequest): Response<Unit>
+
 }
