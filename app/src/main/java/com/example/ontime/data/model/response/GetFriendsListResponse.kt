951ae346a39d06c1d1f39ec41bb0232b5228c934
pp.navillera <pp.navillera@gmail.com>
2024-11-22 01:39:01 +0900

    Feat: define data class for GetFriends Response

---
+++ app/src/main/java/com/example/ontime/data/model/response/GetFriendsListResponse.kt
@@ -0,0 +1,8 @@
+package com.example.ontime.data.model.response
+
+data class GetFriendsListResponse(
+    val id: String,
+    val phoneNumber: String,
+    val name: String,
+    val tardinessRate: Int,
+)
