659f6be5a98bf5a17644313da871dbc8780e7fb0
pp.navillera <pp.navillera@gmail.com>
2024-11-21 22:45:54 +0900

    Feat: add AddFriends navigation route

--- app/src/main/java/com/example/ontime/navigation/NavGraph.kt
+++ app/src/main/java/com/example/ontime/navigation/NavGraph.kt
@@ -3,4 +3,5 @@ sealed class Screen(val route: String) {
     object Main : Screen("main")
     object TeamFormation : Screen("teamFormation")
     object FriendSelection : Screen("friendSelection")
+    object AddFriends : Screen("addFriends")
 }
