12a5c0843dbebd7ce648b8f8e497c757b432cec6
pp.navillera <pp.navillera@gmail.com>
2024-11-22 01:48:39 +0900

    Feat: define Requester data class Feat: define GET getFriendshipRequestList API

--- app/src/main/java/com/example/ontime/data/api/FriendApi.kt
+++ app/src/main/java/com/example/ontime/data/api/FriendApi.kt
@@ -2,6 +2,7 @@ package com.example.ontime.data.api
 
 import com.example.ontime.data.model.request.AddFriendRequest
 import com.example.ontime.data.model.response.FriendResponse
+import com.example.ontime.data.model.response.FriendshipRequestListResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,5 +15,8 @@ interface FriendApi {
 
     @POST("/api/v1/friendship")
     suspend fun addFriend(@Body request: AddFriendRequest): Response<Unit>
+
+    @GET("/api/v1/friendship/request/{userId}")
+    suspend fun getFriendshipRequestList(@Path("userId") userId: String): Response<List<FriendshipRequestListResponse>>
 
 }
---
+++ app/src/main/java/com/example/ontime/ui/friend/requestAccpet/Requester.kt
@@ -0,0 +1,9 @@
+package com.example.ontime.ui.friend.requestAccpet
+
+data class Requester(
+    val id: String,
+    val phoneNumber: String,
+    val name: String,
+    val tardinessRate: Int
+)
+
