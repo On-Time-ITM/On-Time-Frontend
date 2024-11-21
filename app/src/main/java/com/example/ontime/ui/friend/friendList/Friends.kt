303b6e075aebb9378a4720056d7f83f476ab11fc
pp.navillera <pp.navillera@gmail.com>
2024-11-22 01:40:20 +0900

    Feat: define Friends data class

---
+++ app/src/main/java/com/example/ontime/ui/friend/friendList/Friends.kt
@@ -0,0 +1,9 @@
+package com.example.ontime.ui.friend.friendList
+
+data class Friends(
+    val id: String,
+    val name: String,
+    val phoneNumber: String,
+    val tardinessRate: Float,
+    val profileImage: String
+)
