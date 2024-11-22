2f9b3cf5f5c81aceb50ff0fc277d997b809bb6ff
pp.navillera <pp.navillera@gmail.com>
2024-11-22 15:37:25 +0900

    Feat: define GET getFriendshipRequestList API

--- app/src/main/java/com/example/ontime/data/api/FriendApi.kt
+++ app/src/main/java/com/example/ontime/data/api/FriendApi.kt
@@ -1,11 +1,13 @@
 package com.example.ontime.data.api
 
 import com.example.ontime.data.model.request.AddFriendRequest
+import com.example.ontime.data.model.request.FriendshipRequestAcceptRequest
 import com.example.ontime.data.model.response.FriendResponse
 import com.example.ontime.data.model.response.FriendshipRequestListResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -19,4 +21,8 @@ interface FriendApi {
     @GET("/api/v1/friendship/request/{userId}")
     suspend fun getFriendshipRequestList(@Path("userId") userId: String): Response<List<FriendshipRequestListResponse>>
 
+    @PATCH("/api/v1/friendship/accept")
+    suspend fun acceptFriendshipRequest(
+        @Body request: FriendshipRequestAcceptRequest
+    ): Response<Unit>
 }
