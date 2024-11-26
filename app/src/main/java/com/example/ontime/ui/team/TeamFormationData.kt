c8de5f03a22c92b409d3847e57add6806e5d96c9
pp.navillera <pp.navillera@gmail.com>
2024-11-27 02:55:11 +0900

    Feat: Add ISO 8601 datetime field to TeamFormationData
    - Add meetingDateTime field for ISO 8601 formatted date/time

--- app/src/main/java/com/example/ontime/ui/team/TeamFormationData.kt
+++ app/src/main/java/com/example/ontime/ui/team/TeamFormationData.kt
@@ -9,6 +9,7 @@ data class TeamFormationData(
     val latitude: Double = 0.0,
     val longitude: Double = 0.0,
     val bankAccount: AccountData? = null,  // AccountData를 포함
+    val meetingDateTime: String = "", // ISO 8601 형식의 날짜/시간
     val date: String = "",
     val time: String = "",
 )
