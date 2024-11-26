66e16daaecc21895a6f1cb95c861826e72d04a36
pp.navillera <pp.navillera@gmail.com>
2024-11-26 13:54:56 +0900

    Feat: add security-crypto dependency for encrypted storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,5 @@ dependencies {
     implementation(libs.maps.compose)
     implementation(libs.play.services.maps)
     implementation(libs.play.services.location)
+    implementation(libs.androidx.security.crypto)
 }
