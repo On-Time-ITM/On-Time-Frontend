b9b0d135c2c808d0ded609f9ac676975222b77b4
pp.navillera <pp.navillera@gmail.com>
2024-11-27 02:56:08 +0900

    Feat: Implement time selection in CalendarViewModel

--- app/src/main/java/com/example/ontime/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/ontime/ui/calendar/CalendarViewModel.kt
@@ -5,6 +5,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import java.time.LocalDate
+import java.time.LocalTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,11 +23,21 @@ class CalendarViewModel @Inject constructor() : ViewModel() {
 //            }
 //        }
 //    }
+
+
+    // 선택된 날짜 상태
     private val _selectedDate = MutableStateFlow(LocalDate.now())
     val selectedDate = _selectedDate.asStateFlow()
+
+    // 선택된 시간 상태 추가
+    private val _selectedTime = MutableStateFlow(LocalTime.of(12, 0)) // 기본값 12:00
+    val selectedTime = _selectedTime.asStateFlow()
 
     fun updateDate(date: LocalDate) {
         _selectedDate.value = date
     }
 
+    fun updateTime(hour: Int, minute: Int) {
+        _selectedTime.value = LocalTime.of(hour, minute)
+    }
 }
