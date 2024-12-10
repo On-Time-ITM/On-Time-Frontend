27e6e6537d3c3640470512d331db5e372e6b83c2
pp.navillera <pp.navillera@gmail.com>
2024-12-10 21:04:18 +0900

    Feat: define CreateMeetingRequest & Response data class

---
+++ app/src/main/java/com/example/ontime/data/model/request/CreateMeetingRequest.kt
@@ -0,0 +1,18 @@
+package com.example.ontime.data.model.request
+
+data class CreateMeetingRequest(
+    val name: String,
+    val meetingDateTime: String, // ISO 8601 형식의 문자열
+    val location: Location?,
+    val lateFee: Int,
+    val accountInfo: AccountInfoRequest,
+    val hostId: String, // UUID를 String으로 수정
+    val participantIds: List<String>, // UUID를 String으로 수정
+    val profileImage: String
+)
+
+data class Location(
+    val latitude: Double,
+    val longitude: Double,
+    val address: String
+)
---
+++ app/src/main/java/com/example/ontime/data/model/response/CreateMeetingResponse.kt
@@ -0,0 +1,5 @@
+package com.example.ontime.data.model.response
+
+data class CreateMeetingResponse(
+    val meetingId: String
+)
