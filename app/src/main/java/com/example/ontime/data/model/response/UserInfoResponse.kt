282fe4648bcba406eee3b7c6f4e3ba83ff5c9a2c
pp.navillera <pp.navillera@gmail.com>
2024-12-13 00:46:59 +0900

    Feat: update UserInfoResponse adding statistics

--- app/src/main/java/com/example/ontime/data/model/response/UserInfoResponse.kt
+++ app/src/main/java/com/example/ontime/data/model/response/UserInfoResponse.kt
@@ -1,7 +1,10 @@
 package com.example.ontime.data.model.response
 
+import com.example.ontime.data.model.auth.UserStatistics
+
 data class UserInfoResponse(
     val id: String,
     val name: String,
-    val phoneNumber: String
+    val phoneNumber: String,
+    val statistics: UserStatistics
 )
