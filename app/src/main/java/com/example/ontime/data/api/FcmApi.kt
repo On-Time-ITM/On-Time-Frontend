88b2bde4fbe82659341fca95d15f14983107b969
pp.navillera <pp.navillera@gmail.com>
2024-12-13 00:54:06 +0900

    Feat: implement FCM API

---
+++ app/src/main/java/com/example/ontime/data/api/FcmApi.kt
@@ -0,0 +1,12 @@
+package com.example.ontime.data.api
+
+import com.example.ontime.data.model.request.FcmTokenRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+interface FcmApi {
+    @POST("/api/v1/messages/token")
+    suspend fun saveToken(@Body request: FcmTokenRequest): Response<Unit>
+}
