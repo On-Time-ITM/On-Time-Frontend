d5a280bb6b74c91889069b3609de87923e6061de
pp.navillera <pp.navillera@gmail.com>
2024-12-13 01:07:11 +0900

    Feat: integrate Firebase Messaging and notification permission handling

--- app/src/main/java/com/example/ontime/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/ontime/ui/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ontime.ui.main
 
-import MainNavigation
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,13 +14,37 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.ontime.ui.theme.OnTimeTheme
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val token = task.result
+                    Log.d("FCM_TEST", "Token: $token")
+                } else {
+                    Log.e("FCM_TEST", "Token failed", task.exception)
+                }
+            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    1
+                )
+            }
+        }
 //        enableEdgeToEdge()
         setContent {
             OnTimeTheme {
