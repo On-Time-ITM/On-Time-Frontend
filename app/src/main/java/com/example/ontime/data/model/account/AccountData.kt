927084be0209aea81cb52be13ddd861f5a47608d
pp.navillera <pp.navillera@gmail.com>
2024-12-13 01:13:36 +0900

    Feat: update AccountData Model

--- app/src/main/java/com/example/ontime/data/model/account/AccountData.kt
+++ app/src/main/java/com/example/ontime/data/model/account/AccountData.kt
@@ -3,5 +3,6 @@ package com.example.ontime.data.model.account
 data class AccountData(
     val bankName: String,
     val accountNumber: String,
-    val accountHolder: String
+    val accountHost: String,
+    val lateFee: Int
 )
